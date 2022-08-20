461c42904b29317e5fbb4dc9f45d82fb09c418bf
Carlos Valdez <cvaldez.scse@gmail.com>
2022-08-19 23:17:40 -0500

    Break point

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,23 +1,46 @@
+#!/bin/bash
 
 # Install XCode
   # Download from https://apps.apple.com/us/app/xcode/id497799835?mt=12
   # Install the CLI tools
-    xcode-select --install
+  xcode-select --install
     
 # Install Homebrew    
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/carlos/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
-brew install -- cask iterm2
+brew install --cask iterm2
+brew install wget
+
 # update iterm2 settings -> colors, keep directory open new shell, keyboard shortcuts
-brew install bash # latest version of bash
-# set brew bash as default shell
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo $SHELL
+
+# Some funny CLI integrations
+brew install cowsay
+brew install fortune
+cowsay -f tux hello $USER
+
+# Additional software
 brew install fortune
 brew install cowsay 
 brew install git
 brew install vcprompt
+
+# Node Version Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo "CARLOS LOG >>>> : You need to restart the CLI prompt to activate nvm"
+
+
 # update bash_profile
-brew cask install spectacle
-brew cask install alfred
+brew install --cask spectacle
+# brew install --cask alfred
+brew install bind
+
+
 # set CMD+space to launch alfred
 brew cask install firefox
 # install nvm/node
@@ -26,5 +49,11 @@ nvm install stable
 mkdir ~/workspace
 npm install -g lite-server eslint
 brew cask install visual-studio-code
+
+# Configure the finder
+# Install breaktimr and flux
+
+
+
 # update vscode settings
 # install vscode extensions 
